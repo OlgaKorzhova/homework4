051da5cee5759130c8f441e4c39e803e9ac15b62
Olga <muanaire@gmail.com>
2023-01-18 20:33:38 +0000

    Задача 27

---
+++ task27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, принимает на вход число и выдает сумму цифр в числе
+
+int num = int.Parse(Console.ReadLine());
+
+int result = 0;
+while (num != 0)
+{
+   result +=  num % 10;
+   num /= 10;
+}
+
+Console.WriteLine ($"{result}");
