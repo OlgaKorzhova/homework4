a5bb3924bac0c497f973fec51c0e52a43824edd7
Olga <muanaire@gmail.com>
2023-01-18 20:25:12 +0000

    Задача 25

---
+++ task25/Program.cs
@@ -0,0 +1,18 @@
+﻿/// Цикл, принимает на вход 2 числа A и B, и возводит A в степень B
+
+int numA = int.Parse(Console.ReadLine());
+int numB = int.Parse(Console.ReadLine());
+
+
+int result = numA;
+for (int i = 2; i <= numB; i++)
+{
+   result *=  numA;
+}
+
+Console.WriteLine ($"{numA} ^ {numB} = {result}");
+
+
+
+    
+
